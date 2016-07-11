fb9edeeddec25c13ea5d7a82e0a7dc2122baeee7
plogue <prc-lh138-06@PRC-LH138-06s-iMac.local>
2016-07-11 16:32:42 -0500

    Geo

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let base:Int = 2
+
+let height:Int = 5
+
+let width:Int = 10
+
+let length:Int = 20
+
+
+// determine area of shape
+
+var Triangle = ((1 / 2) * (base * height))
+
+var Rectangle = length * width
+
+var parallelogram = base * height
+
+
+print(Triangle, terminator:"")
+print(Rectangle,terminator:"" )
+print(parallelogram, terminator:"")
